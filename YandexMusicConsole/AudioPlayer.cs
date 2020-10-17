64b86bc5e4d30855231ca8c2cadec203eb5331e9
Sergey Samodurov <god-sergey@yandex.ru>
2020-10-17 17:05:48 +0300

    Initial
    initial commit

---
+++ YandexMusicConsole/AudioPlayer.cs
@@ -0,0 +1,79 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Yandex.Music.Api;
+using Yandex.Music.Api.Models.Track;
+using Yandex.Music.Api.Common;
+
+namespace YandexMusicConsole
+{
+    class AudioPlayer
+    {
+        private string _title;
+        private TimeSpan _time;
+        private int TotalPosition = 0;
+        private AuthStorage _authStorage;
+        private YTrack _track;
+        private TimeSpan _currentTime;
+
+        public AudioPlayer(AuthStorage authStorage, YTrack track)
+        {
+            _authStorage = authStorage;
+            _track = track;
+            _currentTime = new TimeSpan();
+        }
+
+        public void Play()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                using (var audioFile = new AudioFileReader($"{_authStorage.Debug.OutputDir}/{_track.Title}.mp3"))
+                using (var outputDevice = new WaveOutEvent())
+                {
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+
+                    SetTrack(_track.Title, audioFile.TotalTime);
+
+                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Console.Clear();
+                        SetCurrentPosition(audioFile.CurrentTime);
+                        Show();
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+
+        public void SetTrack(string title, TimeSpan time)
+        {
+            _title = title;
+            _time = time;
+
+            if (_title.Length >= 12)
+                _title = $"{_title.Substring(0, 12)}...";
+        }
+
+        public void SetCurrentPosition(TimeSpan time)
+        {
+            _currentTime = time;
+
+            var current = time.TotalSeconds;
+            var total = 58 / _time.TotalSeconds;
+
+            //TotalPosition = (int)((_time.TotalSeconds / 100) * time.TotalSeconds);
+            TotalPosition = (int)(total * current);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine();
+            Console.WriteLine($":.\t{_title}\t{new string(' ', 30)}\t\t{_currentTime.Minutes}:{_currentTime.Seconds} | {_time.Minutes}:{_time.Seconds}");
+            Console.WriteLine($":.\t|{new string('-', TotalPosition)}>{new string('.', 58 - TotalPosition)}|");
+        }
+    }
+}
---
+++ YandexMusicConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using Yandex.Music.Api;
+using Yandex.Music.Api.Common;
+using Yandex.Music.Api.Models.Track;
+
+namespace YandexMusicConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login;
+            string password;
+            if (args.Length > 0)
+            {
+                login = args[0];
+                password = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Введите логин");
+                login = Console.ReadLine();
+                Console.WriteLine("Введите пароль");
+                password = Console.ReadLine();
+                Console.Clear();
+            }
+            DebugSettings debugSettings = new DebugSettings(@"C:\yandex_music", @"C:\yandex_music\log.txt");
+            debugSettings.Clear();
+            AuthStorage authStorage = new AuthStorage(debugSettings);
+            
+            authStorage.User.Login = login;
+            var api = new YandexMusicApi();
+            api.User.Authorize(authStorage, authStorage.User.Login, password);
+            if (authStorage.IsAuthorized)
+            {
+                Console.WriteLine("Успешная авторизация");
+            }
+            else
+            {
+                Console.WriteLine("Неверный логин или пароль");
+                return;
+            }
+
+            Console.WriteLine("Какой трек Вы хотите прослушать?");
+            string searchingQuery = Console.ReadLine();
+            var search = api.Search.Track(authStorage, searchingQuery);
+
+            if (search.Result.Tracks == null)
+            {
+                Console.WriteLine("По Вашему запросу ничего не найдено :(");
+                return;
+            }
+
+            int count = 1;
+            foreach (var item in search.Result.Tracks.Results)
+            {
+                Console.WriteLine($"{count}. {item.Artists[0].Name} - {item.Title}");
+                count++;
+            }
+
+            Console.WriteLine("Выберите трек из списка:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice < 1 || choice > 20)
+            {
+                Console.WriteLine("Вы ввели неверное число.");
+                return;
+            }
+
+            YTrack track = api.Track.Get(authStorage, search.Result.Tracks.Results[choice - 1].Id).Result[0];
+            
+            var fileLink = api.Track.GetFileLink(authStorage, track);
+            
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(fileLink, @$"{debugSettings.OutputDir}\{track.Title}.mp3");
+            }
+            var audio = new AudioPlayer(authStorage, track);
+            audio.Play();
+
+            Console.ReadKey();
+        }
+    }
+}
